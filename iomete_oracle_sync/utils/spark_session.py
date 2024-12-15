421fc5e537f999fe9563e5177cd6d8f7c9ef04f1
abhishekpathania <abhishek@iomete.com>
2024-12-15 22:21:53 +0400

    package handling fix

--- iomete_oracle_sync/utils/spark_session.py
+++ iomete_oracle_sync/utils/spark_session.py
@@ -1,9 +1,8 @@
-from iomete_oracle_sync.utils.certificate_handler import CustomChannelBuilder
-
 APP_NAME = "JDBC migration"
 
 
 def get_custom_spark_connect_session(sc_url: str, certificate: str):
+  from iomete_oracle_sync.utils.certificate_handler import CustomChannelBuilder
   from pyspark.sql.connect.session import SparkSession
 
   return SparkSession.builder.appName(APP_NAME).channelBuilder(CustomChannelBuilder(sc_url, certificate)).getOrCreate()
