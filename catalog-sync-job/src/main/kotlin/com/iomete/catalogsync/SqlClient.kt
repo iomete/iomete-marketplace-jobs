a9cc3b8e5545d7f8110601884a4a391004017912
Fuad Musayev <fuadmusayev_@outlook.com>
2025-01-12 10:31:39 +0400

    Multi Domain and Namespace support

--- catalog-sync-job/src/main/kotlin/com/iomete/catalogsync/SqlClient.kt
+++ catalog-sync-job/src/main/kotlin/com/iomete/catalogsync/SqlClient.kt
@@ -10,6 +10,6 @@ import jakarta.ws.rs.PathParam
 @RegisterRestClient(configKey = "core-service")
 interface SqlClient {
     @GET
-    @Path("/domains/{domain}/spark/settings/catalog-names")
+    @Path("/api/internal/domains/{domain}/spark/settings/catalog-names")
     fun catalogs(@PathParam("domain") domainIdentifier: String): Set<String>
 }
