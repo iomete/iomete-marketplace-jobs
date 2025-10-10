641995fbd3039068c2255d390505ad7590b11e8e
Aslan Bakirov <aslanbekirov@gmail.com>
2025-10-10 09:27:24 +0100

    Upgrade dependencies for vulnerability fix (#55)

--- iomete-catalog-sync/catalog-sync-job/build.gradle.kts
+++ iomete-catalog-sync/catalog-sync-job/build.gradle.kts
@@ -25,6 +25,14 @@ dependencies {
     testImplementation("org.scala-lang:scala-library:2.12.10")
     testRuntimeOnly("org.scala-lang:scala-library:2.12.10")
 
+    // Force specific versions for transitive dependencies
+    implementation("org.apache.parquet:parquet-avro:1.15.2")
+    implementation("org.apache.avro:avro:1.11.4")
+    implementation("com.mysql:mysql-connector-j:8.2.0")
+    implementation("com.google.protobuf:protobuf-java:3.25.5")
+    implementation("org.apache.thrift:libthrift:0.14.0")
+    implementation("io.quarkus:quarkus-core:3.8.4")
+
     // runtimeOnly("org.apache.logging.log4j:log4j-jul:2.20.0")
     // implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     // implementation("org.apache.logging.log4j:log4j-api:2.20.0")
