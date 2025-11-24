629f7b843f7c6712c48692653033b9fc58a8b0f2
ujjawal <khareu460@gmail.com>
2025-11-24 17:32:11 +0530

    cleanup
    Signed-off-by: ujjawal <khareu460@gmail.com>

--- ras-onboarding/ras_onboarding/common/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-import requests
-
-
-def make_request(url: str, method: str,headers: Dict[str, str], params: Dict[str, str], data: Dict[str, str]):
-    resp = requests.request(method, url, headers=headers, params=params, data=data)
-    resp.raise_for_status()
-    return resp.json()
