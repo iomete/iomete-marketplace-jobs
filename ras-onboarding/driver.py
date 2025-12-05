cda3f01988ffb58c6043e65c36f2c7510e5b93cf
ujjawal <khareu460@gmail.com>
2025-12-05 19:18:08 +0530

    refactor

--- ras-onboarding/driver.py
+++ ras-onboarding/driver.py
@@ -1,16 +1,10 @@
-import os
 from pyspark.sql import SparkSession
 
 from ras_onboarding.common.config import get_config
 from ras_onboarding.main import start_job
 
-migration_type_env = os.getenv("MIGRATION_TYPE", "asset").lower()
-
 config = get_config()
 
-migration_type = config.get("migration", {}).get("migration_type", "asset")
-app_name = f"RAS Onboarding Migration - {migration_type.capitalize()}"
-
-spark = SparkSession.builder.appName(app_name).getOrCreate()
+spark = SparkSession.builder.appName("Asset RAS Onboarding Migration").getOrCreate()
 
 start_job(spark, config)
